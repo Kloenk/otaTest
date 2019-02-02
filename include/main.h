38407f25b0ae97cfe1860d33c52ffaaaacd81510
Finn Behrens <Kloenk@gmx.de>
2019-02-02 20:57:28 +0100

    init

---
+++ include/main.h
@@ -0,0 +1,18 @@
+//
+// Created by finn on 2/2/19.
+//
+
+#ifndef ESP8266_MAIN_H
+#define ESP8266_MAIN_H
+
+#define SERIALBAUT 115200
+
+#define SERVOPORT 4
+#define WIFI_NAME "TT-WLAN"
+#define WIFI_PASS "Tz5?8:?8nfzjaq@"
+
+
+void setup();
+void loop();
+
+#endif //ESP8266_MAIN_H
